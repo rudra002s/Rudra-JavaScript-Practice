6063615d301b54dfa92bcda8f77108d32c8f8cfe
Rudra Sharma <rudras.1902@gmail.com>
2026-01-28 13:35:30 +0530

    learnt about invalid variable name

--- variables_datatypes/script.js
+++ variables_datatypes/script.js
@@ -3,6 +3,8 @@ console.log("Hello, World!");
 var a=5;
 var b=6;
 var c= "Rudra"
+var _a="Ritu"
+//var 55a="Rutu"  //invalid variable name
 
 console.log(a+b);
-console.log(typeof a,typeof b,typeof c) //tells the type of variables used
+console.log(typeof a,typeof b,typeof c)  //tells the type of variables used
