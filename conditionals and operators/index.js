e33b9078d37944b0662a744ee487ac941978e331
Rudra Sharma <rudras.1902@gmail.com>
2026-01-30 23:54:13 +0530

    learning about operators

--- conditionals and operators/index.js
+++ conditionals and operators/index.js
@@ -1,18 +1,32 @@
 console.log("Hello i am rudra")
 
-let age=7;
-let grace=2;
+let age=18;
+// let grace=2;
 
-console.log(age+grace)
-console.log(age-grace)
-console.log(age*grace)
-console.log(age/grace)
-console.log(age**grace)
+// age +=grace //assignment operator
 
-if((age+grace)>18){
+// console.log(age)
+// console.log(age+grace)
+// console.log(age-grace)
+// console.log(age*grace)
+// console.log(age/grace)
+// console.log(age**grace)
+
+// if(age>18){
+//     console.log("you can drive");
+// }
+if(age==18){ //comparison operators
     console.log("you can drive");
 }
 
 else{
     console.log("you cannot drive");
 }
+
+// if((age+grace)>18){
+//     console.log("you can drive");
+// }
+
+// else{
+//     console.log("you cannot drive");
+// }
