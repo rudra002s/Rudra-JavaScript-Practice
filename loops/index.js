1dc484fe43f6feb8f193fcb6c9aff3b5626f58d8
Rudra Sharma <rudras.1902@gmail.com>
2026-02-02 20:04:57 +0530

    add initial loop examples and comments in index.js

---
+++ loops/index.js
@@ -0,0 +1,14 @@
+console.log("i am learning loops")
+
+let a=1;
+console.log(a)
+console.log(a+1)
+console.log(a+2)
+console.log(a+3)
+
+// there are five types of loops
+// for loop
+// for in loop
+// for of loop
+// while loop
+// do while loop
