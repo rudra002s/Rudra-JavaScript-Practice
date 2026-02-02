94b89db2f7b8b3264beb19f1b6ff30d109c3d95e
Rudra Sharma <rudras.1902@gmail.com>
2026-02-02 20:13:35 +0530

    learnt about for loops

--- loops/index.js
+++ loops/index.js
@@ -1,10 +1,12 @@
 console.log("i am learning loops")
 
 let a=1;
-console.log(a)
-console.log(a+1)
-console.log(a+2)
-console.log(a+3)
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+// console.log(a+3)
+
+//loops can be used to print numbers you cannot type number till n manually so we need loops
 
 // there are five types of loops
 // for loop
@@ -12,3 +14,9 @@ console.log(a+3)
 // for of loop
 // while loop
 // do while loop
+
+for (let i = 0; i < 500; i++){
+    console.log(a+i)
+}
+    
+// in this we first initialise then put condition then it prints in console log
