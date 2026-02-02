7abc2388ac37372efbe2711b51da1ec640cad21f
Rudra Sharma <rudras.1902@gmail.com>
2026-02-02 23:42:12 +0530

    add sum function to calculate and log sums of numbers

--- functions/index.js
+++ functions/index.js
@@ -5,3 +5,17 @@ function rudra(name) {
 
 rudra("ritu")
 rudra("aman")
+
+
+function sum(a,b) {
+    // console.log(a+b)
+    return a+b
+}
+
+result1=sum(3,5)
+result2=sum(34,5)
+result3=sum(3,95)
+
+console.log("The sum of these numbers is:",result1)
+console.log("The sum of these numbers is:",result2)
+console.log("The sum of these numbers is:",result3)
