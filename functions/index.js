3b1b23b24d576862308a99496ef0268b46df6eaa
Rudra Sharma <rudras.1902@gmail.com>
2026-02-02 23:50:20 +0530

    update sum function to include optional third parameter and adjust calculations

--- functions/index.js
+++ functions/index.js
@@ -7,14 +7,15 @@ rudra("ritu")
 rudra("aman")
 
 
-function sum(a,b) {
+function sum(a,b,c=1) {
+    //here by giving the value of c, c will always be added to the operations called below
     // console.log(a+b)
-    return a+b
+    return a+b+c
 }
 
 result1=sum(3,5)
 result2=sum(34,5)
-result3=sum(3,95)
+result3=sum(3,95,2) //but yaha pr c apni value 1 ki jagah 2 lega because hamne yaha value assign kari hai
 
 console.log("The sum of these numbers is:",result1)
 console.log("The sum of these numbers is:",result2)
