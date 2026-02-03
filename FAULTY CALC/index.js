e95afe598ee7ed74cbeeaf6c50037474dea83f01
Rudra Sharma <rudras.1902@gmail.com>
2026-02-03 11:33:11 +0530

    add initial comments and structure for faulty calculator project

---
+++ FAULTY CALC/index.js
@@ -0,0 +1,12 @@
+/*create a faulty calc using js
+
+1) take input from use
+2) performs wrong operations as follows;
++___-
+*___+
+-___/
+/___**
+
+it performs wrong operation 10% of the times
+
+ */
