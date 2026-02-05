a8c6c7fce991ef1b76cf9bd23e288b50b8c83f9f
Rudra Sharma <rudras.1902@gmail.com>
2026-02-05 23:29:04 +0530

    update index.html structure and enhance script.js for DOM manipulation

--- DOM/script.js
+++ DOM/script.js
@@ -1,10 +1,16 @@
 console.log("Hello world")
 console.log(document.body)
-console.log(document.body.childNodes)
-console.log(document.body.childNodes[1])
-console.log(document.body.childNodes[1].childNodes)
-console.log(document.body.childNodes[1].firstChild) //it is used to get first child of the node
-console.log(document.body.childNodes[1].lastChild)
-console.log(document.body.childNodes[1].firstElementChild) //it is used to get the first main element child that is div having box1 and same can be done for last child as well
+// console.log(document.body.childNodes)
+// console.log(document.body.childNodes[1])
+// console.log(document.body.childNodes[1].childNodes)
+// console.log(document.body.childNodes[1].firstChild) //it is used to get first child of the node
+// console.log(document.body.childNodes[1].lastChild)
+// console.log(document.body.childNodes[1].firstElementChild) //it is used to get the first main element child that is div having box1 and same can be done for last child as well
 
-console.log(document.body.childNodes[1].firstElementChild.style.backgroundColor="yellow") //used css properties as well
+// console.log(document.body.childNodes[1].firstElementChild.style.backgroundColor="yellow") //used css properties as well
+
+console.log(document.body.firstElementChild)
+console.log(document.body.firstElementChild.children[0])
+console.log(document.body.firstElementChild.children[1])
+console.log(document.body.firstElementChild.children[1].nextElementSibling)
+console.log(document.body.firstElementChild.children[1].previousElementSibling)
