680c548892038981c1090f7586e9d5be301c3b94
Rudra Sharma <rudras.1902@gmail.com>
2026-02-05 21:49:24 +0530

    update index.html structure and create script.js for DOM manipulation

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+console.log("Hello world")
+console.log(document.body)
+console.log(document.body.childNodes)
+console.log(document.body.childNodes[1])
+console.log(document.body.childNodes[1].childNodes)
+console.log(document.body.childNodes[1].firstChild)
+console.log(document.body.childNodes[1].lastChild)
