46d735c637e35d5d07452e2a62ec1c7b8023a594
Rudra Sharma <rudras.1902@gmail.com>
2026-02-05 10:18:06 +0530

    accessing the elements of a string and knowing its length

---
+++ strings/index.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Strings")
+
+let a="Rudra"
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5]) //this will show undefined as there are only 5 letters is rudra from 0 to 4
+
+console.log(a.length) //tells us the length of an array
