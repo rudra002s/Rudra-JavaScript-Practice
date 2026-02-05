bad9f9d9da58eba5e220f94883b344a5dff6d324
Rudra Sharma <rudras.1902@gmail.com>
2026-02-05 11:37:38 +0530

    add initial list of adjectives, shop names, and additional words for business name generator

---
+++ business_name_generator/index.js
@@ -0,0 +1,16 @@
+// create a business name generator by combining list of adjectives and shop name and another word
+
+// list of adjectives:
+// crazy
+// amazing
+// fire
+
+// shop name:
+// engine 
+// food 
+// garments 
+
+// another word:
+// bros 
+// limited 
+// hub 
