656d9fb018da61a4219c717e22bca4b7a528fd67
Rudra Sharma <rudras.1902@gmail.com>
2026-02-06 20:41:37 +0530

    update index.html and index.js to enhance box styling and improve attribute logging

--- Inserting and_removing_elements/index.js
+++ Inserting and_removing_elements/index.js
@@ -3,6 +3,7 @@
 console.log(document.querySelector(".box").innerHTML) //to see what is inside box
 console.log(document.querySelector(".container").innerHTML) //to see what is inside container
 console.log(document.querySelector(".container").innerText) //to see the inside text of container
+console.log(document.querySelector(".box").innerHTML="Hey i am Box1") //updates the inner element of the first div having the class box
 
 //outer html
 
@@ -10,3 +11,9 @@ console.log(document.querySelector(".container").outerHTML)
 console.log(document.querySelector(".container").tagName) //gives name for particular tag
 console.log(document.querySelector(".container").nodeName) //gives name for particular node
 console.log(document.querySelector(".container").textContent)
+
+//attributes
+
+console.log(document.querySelector(".box").hasAttribute("style")) //tells wether the code has style or any other element
+console.log(document.querySelector(".box").getAttribute("style")) //shows what type of styling has been done
+console.log(document.querySelector(".box").setAttribute("style","display:inline")) //changes the style
