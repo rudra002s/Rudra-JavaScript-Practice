196bed39dac90479b1fb727682b45ccfad891ce3
Rudra Sharma <rudras.1902@gmail.com>
2026-02-06 09:52:27 +0530

    update script.js to modify background colors of box elements and log them to the console

--- id_classes_etc/script.js
+++ id_classes_etc/script.js
@@ -5,4 +5,14 @@ console.log("Rudra can do it")
 
 // boxes[2].style.backgroundColor = "yellow"
 
-document.getElementById("red").style.backgroundColor="yellow"
+// document.getElementById("red").style.backgroundColor="yellow"
+
+document.querySelector(".box").style.backgroundColor="orange" //colors first element having box
+
+console.log(document.querySelectorAll(".box")) //it will give us a console and cannot be colored as its a html and it cannot be colored to color this we need for 
+
+document.querySelectorAll(".box").forEach(e => {
+    e.style.backgroundColor="pink"
+});
+
+document.getElementsByTagName("div") //gives element by there tag names
