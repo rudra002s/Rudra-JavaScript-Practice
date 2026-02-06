12a0eb19fdc73dbf6ff9de5bff5f63334aed68ae
Rudra Sharma <rudras.1902@gmail.com>
2026-02-06 20:27:51 +0530

    refactor index.html and index.js for improved structure and readability

---
+++ Inserting and_removing_elements/index.js
@@ -0,0 +1,12 @@
+//inner html
+
+console.log(document.querySelector(".box").innerHTML) //to see what is inside box
+console.log(document.querySelector(".container").innerHTML) //to see what is inside container
+console.log(document.querySelector(".container").innerText) //to see the inside text of container
+
+//outer html
+
+console.log(document.querySelector(".container").outerHTML)
+console.log(document.querySelector(".container").tagName) //gives name for particular tag
+console.log(document.querySelector(".container").nodeName) //gives name for particular node
+console.log(document.querySelector(".container").textContent)
