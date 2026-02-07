28e23c4737943c1662bb7be26cfedd01c11db539
Rudra Sharma <rudras.1902@gmail.com>
2026-02-07 10:35:35 +0530

    Refactor HTML structure and CSS styles for card component in index.html, script.js, and style.css for improved layout and readability

--- Dynamic Website Builder/script.js
+++ Dynamic Website Builder/script.js
@@ -1,6 +1,18 @@
-function createCard(title,channelName,views,monthsOld,duration,thumbnail){
-    let HTML=`
-    
+function createCard(title, channelName, views, monthsOld, duration, thumbnail) {
+    let HTML = `
+    <div class="card">
+        <div class="img">
+          <img
+            src="https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDf-xrqgUw55JSfKsoykKVhngkzRA"
+          />
+        </div>
+        <div class="text">
+          <h1>
+            ${title}
+          </h1>
+          <p>${channelName}. ${views} . ${monthsOld}/p>
+        </div>
+      </div>
     `
 }
-createCard("Installing VS Code & How Website Works | Sigma Web Development Course - Tutorial #1","CodeWithHarry","6.8M","September 26, 2023","31:19","https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDf-xrqgUw55JSfKsoykKVhngkzRA")
+createCard("Installing VS Code & How Website Works | Sigma Web Development Course - Tutorial #1", "CodeWithHarry", "6.8M", "September 26, 2023", "31:19", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDf-xrqgUw55JSfKsoykKVhngkzRA")
