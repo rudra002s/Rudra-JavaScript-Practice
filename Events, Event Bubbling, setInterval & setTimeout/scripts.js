e282e01efd3c845b7a06029c494256d46fcb9f2a
Rudra Sharma <rudras.1902@gmail.com>
2026-02-07 22:06:09 +0530

    learning about mouse events

--- Events, Event Bubbling, setInterval & setTimeout/scripts.js
+++ Events, Event Bubbling, setInterval & setTimeout/scripts.js
@@ -1,5 +1,16 @@
-let button=document.getElementById("btn")
+let button = document.getElementById("btn")
 
-button.addEventListener("click",()=>{
-    alert("I was clicked")
+//LIST OF EVENTS
+//https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+
+// button.addEventListener("click",()=>{ //for single click
+button.addEventListener("dblclick", () => {
+    // alert("I was clicked")
+    document.querySelector(".box").innerHTML = "<b> yesss you were clicked </b>" //when btn is pressed it changes the element inside box
+})
+
+button.addEventListener("contextmenu", () => {
+    //it is used for right click
+    alert("RIGHT CLICK")
+     
 })
