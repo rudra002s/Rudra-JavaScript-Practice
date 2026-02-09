0fcaf07ff0948b1f4c14467133f7b28af44aaa42
Rudra Sharma <rudras.1902@gmail.com>
2026-02-09 22:51:14 +0530

    Refactor getData function to use async/await and update main function for better readability

--- Async/Await & Fetch API/index.js
+++ Async/Await & Fetch API/index.js
@@ -1,21 +1,37 @@
-function getData(){
-    return new Promise((resolve,reject)=>{
+async function getData() {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(455)
         }, 3500);
     })
 }
-console.log("Loading Module")
-console.log("random text")
-console.log("Load Data")
+// console.log("Loading Module")
+// console.log("random text")
+// console.log("Load Data")
 
-let data=getData() //yaha par poori script run hogi chahe kitna delay ho yaha par pending show hoga jab tk timer khatam nhi hoga so to stop script until timer is over await will be used
+// let data = getData() //yaha par poori script run hogi chahe kitna delay ho yaha par pending show hoga jab tk timer khatam nhi hoga so to stop script until timer is over await will be used
 
-data.then((v)=>{
+//old way
 
-})
-console.log(data)
+// data.then((v)=>{ //now the script will run after timeout
+// console.log(data)
 
-console.log("Process Data")
+// console.log("Process Data")
 
-console.log("task 2")
+// console.log("task 2")
+// })
+
+//latest way
+async function main() {
+    console.log("Loading Module")
+    console.log("random text")
+    console.log("Load Data")
+    //these three lines will be run instantly
+    let data = await getData()
+    //it will wait for 3.5 seconds to run the async function after that next two lines will run
+    console.log(data)
+    console.log("Process Data")
+    console.log("task 2")
+}
+
+main() //here it is running the function
