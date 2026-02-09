25fc0813c589926e7789a7bd4beaa20cfdcd572c
Rudra Sharma <rudras.1902@gmail.com>
2026-02-09 23:42:07 +0530

    Add initial HTML, CSS, and JavaScript files for Hacker's Terminal simulation

---
+++ Hacker's Terminal/script.js
@@ -0,0 +1,13 @@
+let arr = [
+    "Initializing hacking...",
+    "Reading your files...",
+    "Password files detected...",
+    "Sending all data and personal files to main server...",
+    "Cleaning up..."
+]
+for (const item of text) {
+    addItem(item)
+}
+const addItem=async()=>{
+    
+}
