da44ed5505a1acafa358aa5919be0144c505df06
Rudra Sharma <rudras.1902@gmail.com>
2026-02-09 23:53:10 +0530

    Refactor script.js to implement addItem and randomDelay functions for improved message display

--- Hacker's Terminal/script.js
+++ Hacker's Terminal/script.js
@@ -1,13 +1,26 @@
-let arr = [
+const addItem = async (item) => {
+    await randomDelay()
+    let div=document.createElement("div")
+    div.innerHTML=(item)
+    document.body.append(div)
+}
+
+const randomDelay = () => {
+    return new Promise((resolve, reject) => {
+        timeout = 1 + 6 * Math.random() //for numbers between 1 and 7
+        setTimeout(() => {
+            resolve()
+        }, timeout*1000);
+    })
+}
+let text = [
     "Initializing hacking...",
     "Reading your files...",
     "Password files detected...",
     "Sending all data and personal files to main server...",
     "Cleaning up..."
 ]
+
 for (const item of text) {
     addItem(item)
-}
-const addItem=async()=>{
-    
 }
