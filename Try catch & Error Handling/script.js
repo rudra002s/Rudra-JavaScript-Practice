6b260d71fabf7fc722bbed8b4ef9febe8998f653
Rudra Sharma <rudras.1902@gmail.com>
2026-02-10 22:08:55 +0530

    Add basic addition functionality to script.js with user input prompts

---
+++ Try catch & Error Handling/script.js
@@ -0,0 +1,13 @@
+let a=prompt("Enter first number")
+
+let b=prompt("Enter second number")
+
+/*let sum=a+b 
+this is wrong this will append number instead of adding
+this icode is for string addition
+  */
+
+//code for addition parseInt converts string into integer
+
+let sum=parseInt(a)+parseInt(b)
+console.log("The sum is",sum)
