ec2fa61d382040194e9d94a68611e9fdd4445915
Rudra Sharma <rudras.1902@gmail.com>
2026-02-10 22:26:49 +0530

    Fix formatting in index.html and script.js for consistency

--- Try catch & Error Handling/script.js
+++ Try catch & Error Handling/script.js
@@ -1,6 +1,6 @@
-let a=prompt("Enter first number")
+let a = prompt("Enter first number")
 
-let b=prompt("Enter second number")
+let b = prompt("Enter second number")
 
 /*let sum=a+b 
 this is wrong this will append number instead of adding
@@ -9,5 +9,26 @@ this icode is for string addition
 
 //code for addition parseInt converts string into integer
 
-let sum=parseInt(a)+parseInt(b)
-console.log("The sum is",sum)
+if (isNaN(a) || isNaN(b)) {
+    throw SyntaxError("NOT ALLOWED! PLEASE ENTER A NUMBER")
+}
+/* if instead of number user will type strings it will show NaN instead of showing error
+so in order ot give error we have used this function where || stands for or and using throw error can be shown */
+
+let sum = parseInt(a) + parseInt(b)
+
+//console.log("The sum is",sum) 
+
+//this function is fine
+
+try {
+    console.log("The sum is", sum * x) //sum,x is wrong we are using it to understand the trycatch function 
+} catch (error) {
+    console.log("ERROR HAI BHAI G")
+}
+/* here we are using this to handle the error in try function we have given the line where error was showing and instead of that out custom error will be shown so that user can fix his/her mistake */
+finally {
+    console.log("files are being close and db connection is being closed")
+}
+
+// console.log("The sum is",sum*x) //this will show error that x is not defined
